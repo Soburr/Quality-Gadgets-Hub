86434fb6f2b65e741beb061e8aaee74ff859a17f
Soburr <adebesinbenzenejr99@gmail.com>
2026-07-18 00:05:42 +0100

    feat: brands

--- resources/views/partials/category-nav.blade.php
+++ resources/views/partials/category-nav.blade.php
@@ -1,13 +1,8 @@
 <nav class="category-nav">
     <div class="wrap">
         <a href="{{ route('home') }}"><span class="dot"></span>All Phones</a>
-        <a href="#"><span class="dot"></span>Android</a>
-        <a href="#"><span class="dot"></span>iPhones</a>
-        <a href="#"><span class="dot"></span>Tablets</a>
-        <a href="#"><span class="dot"></span>Smartwatches</a>
-        <a href="#"><span class="dot"></span>Earbuds &amp; Audio</a>
-        <a href="#"><span class="dot"></span>Chargers &amp; Power Banks</a>
-        <a href="#"><span class="dot"></span>Cases &amp; Screen Guards</a>
-        <a href="#"><span class="dot"></span>Refurbished (UK Used)</a>
+        @foreach($navCategories ?? [] as $category)
+            <a href="{{ route('category.show', $category) }}"><span class="dot"></span>{{ $category->label }}</a>
+        @endforeach
     </div>
 </nav>
