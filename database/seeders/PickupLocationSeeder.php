bbf3765925aeeb84771767f83d54469579d74e2c
Soburr <adebesinbenzenejr99@gmail.com>
2026-08-22 09:59:33 +0100

    feat: delivery fees updated

--- database/seeders/PickupLocationSeeder.php
+++ database/seeders/PickupLocationSeeder.php
@@ -9,17 +9,31 @@ class PickupLocationSeeder extends Seeder
 {
     public function run(): void
     {
-        $locations = [
-            ['name' => 'Abule Egba', 'fee' => 1000],
-            ['name' => 'Iyana Ipaja', 'fee' => 1000],
-            ['name' => 'Command', 'fee' => 1200],
+        PickupLocation::query()->delete();
+
+        $tiers = [
+            3000 => ['Ikeja', 'Ojota', 'Maryland', 'MM1 & MM2 Airport', 'Ogba', 'Oshodi', 'Agege', 'Ketu', 'Mile 12', 'Iyana Ipaja'],
+            4000 => ['Omole', 'Magodo', 'Berger', 'Ajao Estate', 'Ogudu', 'Fagba', 'Idimu', 'Egbeda', 'Abule Egba', 'Meiran', 'Isolo', 'Jakande'],
+            5000 => ['Gbagada', 'Ilupeju', 'Mushin', 'Surulere', 'Yaba', 'Ebute Metta'],
+            6000 => ['Marina', 'VI', 'Ikoyi', 'Obalende', 'Oyinbo', 'Igando', 'Ikotun', 'Ijaye', 'Alakuko', 'Alagbodo', 'Costain'],
+            7000 => ['Lagos Island' , 'Idumota', 'Igbo Efon', 'Ikate', 'Lekki Phase 1', 'Ajegunle', 'Apapa', 'Chevron', 'Eleganza', 'Oke Afa'],
+            8000 => ['Ago Palace', 'Festac', 'Mile 2', 'Ikorodu', 'Odongunyan', 'Majidun', 'Owode Onirin', 'VGC'],
+            9000 => ['Akute', 'Opic', 'Iyana School', 'Lasu', 'Iba', 'Maza Maza', 'Abule Ado', 'Ajah', 'Okokomaiko', 'Iyana Isashi', 'Ojoo', 'Iyana Era', 'Ijede', 'Irete', 'Parafa'],
+            10000 => ['Arepo', 'Ajangbadi', 'Sangotedo', 'Badore', 'Langbasa', 'Ogombo', 'Abijo', 'Alaba', 'Imota'],
+            11000 => ['Mowe', 'Toll Gate', 'Ibeju Lekki', 'Agbowa'],
+            12000 => ['Sango Under Bridge', 'Lakowe', 'Eleko', 'Awoyaya', 'Agbara'],
         ];
 
-        foreach ($locations as $i => $location) {
-            PickupLocation::updateOrCreate(
-                ['name' => $location['name']],
-                ['fee' => $location['fee'], 'sort_order' => $i]
-            );
+        $sortOrder = 0;
+
+        foreach ($tiers as $fee => $locations) {
+            foreach ($locations as $name) {
+                PickupLocation::create([
+                    'name' => $name,
+                    'fee' => $fee,
+                    'sort_order' => $sortOrder++,
+                ]);
+            }
         }
     }
 }
